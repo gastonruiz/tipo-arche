275fd2f76bad439a9a5869d274e8ac6457929d65
gaston <gastonruizojeda@gmail.com>
2024-06-02 12:38:19 -0300

    se optimizo las rutas

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,98 +1,74 @@
 import React from 'react'
 import logo from '../Assets/Images/logoArche.jpeg'
-import Home from './Home/Home'
-import Nosotras from './Nosotras/Nosotras'
 import { Link } from 'react-router-dom'
 
 const 
 Navbar = () => {
 
-const NavBarItems = ["Home", "Nosotras", "Cursos","Galeria"] 
-
   return (
       <>
 
-    {/*<div className='grid grid-flow-col auto-cols-max'>
-    <img  src={logo} alt="logo" width={100} height={100}/>
-    <div className='grid grid-flow-col auto-cols-max'>
-      <ul>
-        {NavBarItems.map((item)=>(
-          <li>{item}</li>
-        ))}
-      </ul>
-      </div>
-      </div>*/}
-
-<div class="grid min-h-[140px] w-full place-items-center overflow-x-scroll rounded-lg p-6 lg:overflow-visible">
-  <div class="-m-6 max-h-[768px] w-[calc(100%+48px)] overflow-scroll">
+<div className="grid min-h-[140px] w-full place-items-center overflow-x-scroll rounded-lg p-6 lg:overflow-visible">
+  <div className="-m-6 max-h-[768px] w-[calc(100%+48px)] overflow-scroll">
     <nav
-      class="sticky top-0 z-10 block w-full max-w-full px-4 py-2 text-white bg-rosita border rounded-none shadow-md h-max border-white/80 bg-opacity-80 backdrop-blur-2xl backdrop-saturate-200 lg:px-8 lg:py-4">
-      <div class="flex items-center justify-between text-blue-gray-900">
-        <a href="#"
-          class="mr-4 block cursor-pointer py-1.5 font-sans text-base font-medium leading-relaxed text-inherit antialiased">
-          Tipo Arche
-        </a>
-        <div class="flex items-center gap-4">
-          <div class="hidden mr-4 lg:block">
-            <ul class="flex flex-col gap-2 mt-2 mb-4 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-              <li class="block p-1 font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+      className="sticky top-0 z-10 block w-full max-w-full px-4 py-2 text-white bg-rosita border rounded-none shadow-md h-max border-white/80 bg-opacity-80 backdrop-blur-2xl backdrop-saturate-200 lg:px-8 lg:py-4">
+      <div className="flex items-center justify-between text-blue-gray-900">
+        <Link to={"/"}>
+                <img
+                  className="h-16"
+                  src={logo}
+                  alt="logo"
+                />
+              </Link>
+        <div className="flex items-center gap-4">
+          <div className="hidden mr-4 lg:block">
+            <ul className="flex flex-col gap-2 mt-2 mb-4 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+              <li className="block p-1 font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
               <button
-              class="hidden select-none rounded-lg bg-gradient-to-tr from-gray-900 to-gray-800 py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:inline-block"
+              className="hidden select-none rounded-lg bg-gradient-to-tr from-gray-900 to-gray-800 py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:inline-block"
               type="button">
-              <Link to={"/"} class="flex items-center">
+              <Link to={"/"} className="flex items-center">
                   Home
                 </Link>
             </button>
                 
               </li>
-              <li class="block p-1 font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+              <li className="block p-1 font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
               <button
-              class="hidden select-none rounded-lg bg-gradient-to-tr from-gray-900 to-gray-800 py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:inline-block"
+              className="hidden select-none rounded-lg bg-gradient-to-tr from-gray-900 to-gray-800 py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:inline-block"
               type="button">
-                <Link to={"/Nosotras"} class="flex items-center">
+                <Link to={"/Nosotras"} className="flex items-center">
                   Nosotras
                 </Link>
              
             </button>
               </li>
-              <li class="block p-1 font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+              <li className="block p-1 font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
               <button
-              class="hidden select-none rounded-lg bg-gradient-to-tr from-gray-900 to-gray-800 py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:inline-block"
+              className="hidden select-none rounded-lg bg-gradient-to-tr from-gray-900 to-gray-800 py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:inline-block"
               type="button">
-              <Link to={"/Cursos"} class="flex items-center">
+              <Link to={"/Cursos"} className="flex items-center">
                   Cursos
                 </Link>
             </button>
               </li>
-              <li class="block p-1 font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+              <li className="block p-1 font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
               <button
-              class="hidden select-none rounded-lg bg-gradient-to-tr from-gray-900 to-gray-800 py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:inline-block"
+              className="hidden select-none rounded-lg bg-gradient-to-tr from-gray-900 to-gray-800 py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:inline-block"
               type="button">
-              <Link to={"/Galeria"} class="flex items-center">
+              <Link to={"/Galeria"} className="flex items-center">
                   Galeria
                 </Link>
             </button>
               </li>
             </ul>
           </div>
-          
-          <button
-            class="relative ml-auto h-6 max-h-[40px] w-6 max-w-[40px] select-none rounded-lg text-center align-middle font-sans text-xs font-medium uppercase text-inherit transition-all hover:bg-transparent focus:bg-transparent active:bg-transparent disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:hidden"
-            type="button">
-            <span class="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-              <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" stroke="currentColor"
-                stroke-width="2">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16"></path>
-              </svg>
-            </span>
-          </button>
         </div>
       </div>
     </nav>
     
   </div>
 </div>
-    
     
     </>
   )
