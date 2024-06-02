bfe7d969cd88d4d7ba25c4aed6898049d5b5eb93
gaston <gastonruizojeda@gmail.com>
2024-06-02 18:48:27 -0300

    botones a secciones

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import logo from '../Assets/Images/logoArche.jpeg'
+
 import { Link } from 'react-router-dom'
 
 const 
 Navbar = () => {
 
+
   return (
       <>
 
-<div className="grid min-h-[140px] w-full place-items-center overflow-x-scroll rounded-lg p-6 lg:overflow-visible">
-  <div className="-m-6 max-h-[768px] w-[calc(100%+48px)] overflow-scroll">
+<div className=" ">
+  <div className="grid lg:grid-rows-2">
     <nav
-      className="sticky top-0 z-10 block w-full max-w-full px-4 py-2 text-white bg-rosita border rounded-none shadow-md h-max border-white/80 bg-opacity-80 backdrop-blur-2xl backdrop-saturate-200 lg:px-8 lg:py-4">
-      <div className="flex items-center justify-between text-blue-gray-900">
+      className=" bg-rosita border">
+      <div className="grid grid-cols-2 items-center justify-between">
         <Link to={"/"}>
                 <img
                   className="h-16"
@@ -21,11 +23,13 @@ Navbar = () => {
                 />
               </Link>
         <div className="flex items-center gap-4">
-          <div className="hidden mr-4 lg:block">
-            <ul className="flex flex-col gap-2 mt-2 mb-4 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-              <li className="block p-1 font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+          <div className="">
+            <ul className="flex  items-center gap-6">
+              <li className=" font-sans text-sm ">
               <button
-              className="hidden select-none rounded-lg bg-gradient-to-tr from-gray-900 to-gray-800 py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:inline-block"
+              className="rounded-lg bg-gradient-to-tr from-gray-900 to-gray-800 py-2 px-4 text-center 
+              font-sans text-xs font-bold uppercase shadow-md shadow-gray-900/10 
+              transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85]"
               type="button">
               <Link to={"/"} className="flex items-center">
                   Home
@@ -35,7 +39,9 @@ Navbar = () => {
               </li>
               <li className="block p-1 font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
               <button
-              className="hidden select-none rounded-lg bg-gradient-to-tr from-gray-900 to-gray-800 py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:inline-block"
+              className="rounded-lg bg-gradient-to-tr from-gray-900 to-gray-800 py-2 px-4 text-center 
+              font-sans text-xs font-bold uppercase shadow-md shadow-gray-900/10 
+              transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85]"
               type="button">
                 <Link to={"/Nosotras"} className="flex items-center">
                   Nosotras
@@ -45,7 +51,9 @@ Navbar = () => {
               </li>
               <li className="block p-1 font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
               <button
-              className="hidden select-none rounded-lg bg-gradient-to-tr from-gray-900 to-gray-800 py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:inline-block"
+              className="rounded-lg bg-gradient-to-tr from-gray-900 to-gray-800 py-2 px-4 text-center 
+              font-sans text-xs font-bold uppercase shadow-md shadow-gray-900/10 
+              transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85]"
               type="button">
               <Link to={"/Cursos"} className="flex items-center">
                   Cursos
@@ -54,7 +62,9 @@ Navbar = () => {
               </li>
               <li className="block p-1 font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
               <button
-              className="hidden select-none rounded-lg bg-gradient-to-tr from-gray-900 to-gray-800 py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:inline-block"
+              className="rounded-lg bg-gradient-to-tr from-gray-900 to-gray-800 py-2 px-4 text-center 
+              font-sans text-xs font-bold uppercase shadow-md shadow-gray-900/10 
+              transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85]"
               type="button">
               <Link to={"/Galeria"} className="flex items-center">
                   Galeria
